8b80ff686263c9ca4938cb261d8c56ca90e5a552
Crishdz1919 <163928735+Crishdz1919@users.noreply.github.com>
2024-07-06 02:42:50 -0600

    Videojuego 1.2.1
    Map2 Alex Avance

---
+++ fuentes/Dialogos/EscenarioMarcos/MarcosEs1.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+
+func _on_button_pressed():
+	Trans.change_scene("res://fuentes/Dialogos/EscenarioMarcos/MarcosEs2.tscn")
+	pass # Replace with function body.
+
---
+++ mundo/mundo_3.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_button_pressed():
+	Trans.change_scene("res://fuentes/Dialogos/EscenarioMarcos/MarcosEs4.tscn")
+	pass # Replace with function body.
